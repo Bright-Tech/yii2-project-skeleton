1b7c5a2088ceaf6f4bf4e4466ace754f1914cf36
Sam.Xiao <samxiaotj@gmail.com>
2016-08-11 16:55:21 +0800

    更新错误页面

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -9,19 +9,31 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="error-container">
+	<div class="well">
+		<h1 class="grey lighter smaller">
+			<span class="blue bigger-125"> <i class="ace-icon fa fa-sitemap"></i>
+				<?= Html::encode($this->title) ?>
+			</span>
+		</h1>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
+		<hr />
+		<h3 class="lighter smaller"><?= nl2br(Html::encode($message)) ?></h3>
 
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+		<div>
 
+		</div>
+
+		<hr />
+		<div class="space"></div>
+
+		<div class="center">
+			<a href="javascript:history.back()" class="btn btn-grey"> <i
+				class="ace-icon fa fa-arrow-left"></i> Go Back
+			</a> <a href="#" class="btn btn-primary"> <i
+				class="ace-icon fa fa-tachometer"></i> Dashboard
+			</a>
+		</div>
+	</div>
 </div>
