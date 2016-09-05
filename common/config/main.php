be47052d1aaa39f9a98b74ee37ee434884747426
嘉旭 <836139146@qq.com>
2016-09-05 09:10:50 +0800

    地址修改

--- common/config/main.php
+++ common/config/main.php
@@ -11,6 +11,8 @@ return [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'rules' => [
+            ],
         ],
     ],
 ];
